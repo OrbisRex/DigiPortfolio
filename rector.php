35bb584813ad6877b25e9d1cf9851d00b9dbc7cc
OrbisRex <orbis_rex@outlook.com>
2025-09-06 17:25:18 +0200

    Before Rector applied

---
+++ rector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Bridge\Symfony\Routing\SymfonyRoutesProvider;
+use Rector\Symfony\Configs\Rector\ClassMethod\AddRouteAnnotationRector;
+use Rector\Symfony\Contract\Bridge\Symfony\Routing\SymfonyRoutesProviderInterface;
+
+return RectorConfig::configure()
+    ->withPaths([
+        # __DIR__ . '/config',
+        # __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSymfonyContainerPhp(__DIR__ . '/tests/symfony-container.php')
+    ->registerService(SymfonyRoutesProvider::class, SymfonyRoutesProviderInterface::class)
+    ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withSets([
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+         __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml'
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
