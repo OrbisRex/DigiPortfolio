f5846287151db717521fbe424841b463f2d904ae
OrbisRex <orbis_rex@outlook.com>
2025-09-09 23:25:29 +0200

    Added Symfony Encore

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
